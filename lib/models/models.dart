e9ea270257849f2d6d59ca553f1edd34215cf8c7
daniel <djuanrique@gmail.com>
2025-03-26 18:59:08 -0500

    commit

---
+++ lib/models/models.dart
@@ -0,0 +1,2 @@
+export 'package:movie_app_caicedonia/models/movie.dart';
+export 'package:movie_app_caicedonia/models/now_playing_response.dart';
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,12 +1,22 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = 'f7baea359ce18783053eda3ea74bc4cf';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print(' Movies Provider Inicializado');
     this.getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(this._baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language});
+    final response = await http.get(url);
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print(decodedData);
   }
 }
